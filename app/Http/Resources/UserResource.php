85a64cb398eb308efd20ca0b6d207c47c3c3e5ae
obada <opadaalziede@gmail.com>
2024-06-23 01:30:52 +0300

    return is_deletable column with UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'username' => $this->username,
+            'is_deletable' => (bool) $this->is_deletable,
             'roles' => RoleResource::collection($this->roles)
         ];
     }
