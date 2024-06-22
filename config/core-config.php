5bd6a9062f58b166b9e072ba9b7fec641c9674e2
obada <opadaalziede@gmail.com>
2024-06-22 18:53:53 +0300

    update users username

--- config/core-config.php
+++ config/core-config.php
@@ -13,35 +13,35 @@ return [
         'super_admin' => [
             'first_name' => 'super admin',
             'last_name' => 'super admin',
-            'username' => 'super admin',
+            'username' => 'super_admin',
             'password' => 'password',
             'is_deletable' => false
         ],
         'services_admin' => [
             'first_name' => 'services admin',
             'last_name' => 'services admin',
-            'username' => 'services admin',
+            'username' => 'services_admin',
             'password' => 'password',
             'is_deletable' => true
         ],
         'contacts_admin' => [
             'first_name' => 'contacts admin',
             'last_name' => 'contacts admin',
-            'username' => 'contacts admin',
+            'username' => 'contacts_admin',
             'password' => 'password',
             'is_deletable' => true
         ],
         'authors_admin' => [
             'first_name' => 'authors admin',
             'last_name' => 'authors admin',
-            'username' => 'authors admin',
+            'username' => 'authors_admin',
             'password' => 'password',
             'is_deletable' => true
         ],
         'blogs_admin' => [
             'first_name' => 'blogs admin',
             'last_name' => 'blogs admin',
-            'username' => 'blogs admin',
+            'username' => 'blogs_admin',
             'password' => 'password',
             'is_deletable' => true
         ],
