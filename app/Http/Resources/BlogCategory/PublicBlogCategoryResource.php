814fc3e2ad1d7d7c72ffad6bb9645bdf5cadc5a8
obada <opadaalziede@gmail.com>
2024-07-12 12:39:29 +0300

    add id to PublicBlogCategoryResource

--- app/Http/Resources/BlogCategory/PublicBlogCategoryResource.php
+++ app/Http/Resources/BlogCategory/PublicBlogCategoryResource.php
@@ -15,6 +15,7 @@ class PublicBlogCategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'slug' => $this->getTranslations('slug')
         ];
