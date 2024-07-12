eeded06a34ce5f85525116e3f72256abcd708892
obada <opadaalziede@gmail.com>
2024-07-13 02:03:19 +0300

    implement user_management functionality

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+
+use App\Traits\HasFilters;
+use App\Traits\HasIncludes;
+use Spatie\Sluggable\HasSlug;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasIncludes, HasFilters, HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -36,6 +43,9 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected static array $allowedIncludes = ['roles'];
+    protected static array $allowedFilters = ['first_name', 'last_name', 'email', 'username'];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -46,7 +56,25 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_deletable' => 'boolean',
         ];
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('username')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function canBeDeleted(): bool {
+
+        return $this->is_deletable;
     }
 
     public function roles(): BelongsToMany {
@@ -54,13 +82,16 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class, 'role_user');
     }
 
-    public function assignRole(string $role): void {
+    public function assignRole(string | Role $role): void {
 
-        $roleModel = Role::where('name', $role)->first();
+        if(is_string($role)) {
 
-        if($roleModel) {
+            $role = Role::where('name', $role)->first();
+        }
 
-            $this->roles()->attach($roleModel);
+        if($role) {
+
+            $this->roles()->attach($role);
         }
     }
 
@@ -72,8 +103,28 @@ class User extends Authenticatable
         }
     }
 
-    public function hasRole(string $role): bool {
+    public function hasRole(string | Role $role): bool {
 
-        return $this->roles()->where('name', $role)->exists();
+        $roleName = is_string($role) ? $role : $role->name;
+
+        return $this->roles()->where('name', $roleName)->exists();
+    }
+
+    public function detachRole(string | Role $role): void {
+
+        if(is_string($role)) {
+
+            $role = Role::where('name', $role)->first();
+        }
+
+        $this->roles()->detach($role->id);
+    }
+
+    public function scopeOnlyAdmins(Builder $query): void {
+
+        $query->whereHas('roles', function($rolesQuery) {
+
+            $rolesQuery->where('name', '!=', Role::getSuperAdminRole());
+        });
     }
 }
