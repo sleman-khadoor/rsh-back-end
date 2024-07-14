9a6a023895e2be73762434d6634c1036d49116e2
obada <opadaalziede@gmail.com>
2024-07-14 20:20:11 +0300

    refactor GetRelatedBlogs action

--- app/Actions/Blogs/GetRelatedBlogs.php
+++ app/Actions/Blogs/GetRelatedBlogs.php
@@ -4,30 +4,68 @@ namespace App\Actions\Blogs;
 
 use App\Models\Blog;
 use App\Http\Resources\Blog\PublicBlogResource;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Collection as SupportCollection;
 
 class GetRelatedBlogs {
 
     public function execute(Blog $blog): AnonymousResourceCollection {
 
-        $numberOfBlogsInDB = Blog::count();
+        $numberOfBlogsInDB = Blog::where('lang', $blog->lang)->count();
         $numberOfRequiredRelatedBlogs = config('core-config.number_of_related_blogs');
 
         $relatedBlogsCount = $numberOfRequiredRelatedBlogs <= $numberOfBlogsInDB ? $numberOfRequiredRelatedBlogs : $numberOfBlogsInDB;
 
-        $blogCategoryIDS = $blog->blogCategories()->pluck('blog_category_id')->toArray();
+        $blogCategoryIDS = $blog->blogCategories()->pluck('blog_category_id');
 
 
-        $relatedBlogs = Blog::with('blogCategories')
-                            ->where('id', '!=', $blog->id)
-                            ->where('lang', $blog->lang)
-                            ->whereHas('blogCategories', function($query) use ($blogCategoryIDS) {
-                                    $query->whereIn('blog_categories.id', $blogCategoryIDS);
-                            })
-                            ->orderBy('date', 'DESC')
-                            ->take($relatedBlogsCount)
-                            ->get();
+        $relatedBlogs = $this->getListOfRelatedBlogs($blog->id, $blog->lang, $blogCategoryIDS, $relatedBlogsCount);
+
+        $missingBlogsCount = $relatedBlogsCount - $relatedBlogs->count();
+
+        if ($missingBlogsCount > 0) {
+
+            $randomBlogs = $this->getListOfRandomBlogs($blog->id, $blog->lang, $relatedBlogs, $missingBlogsCount);
+
+            $relatedBlogs = $relatedBlogs->concat($randomBlogs);
+        }
+
 
         return PublicBlogResource::collection($relatedBlogs);
+    }
+
+    private function getListOfRelatedBlogs(
+        int $blogId,
+        string $blogLang,
+        SupportCollection $blogCategoryIDS,
+        int $relatedBlogsCount
+    ): Collection {
+
+        return Blog::where('id', '!=', $blogId)
+                    ->where('lang', $blogLang)
+                    ->whereHas('blogCategories', function($query) use ($blogCategoryIDS) {
+                            $query->whereIn('blog_categories.id', $blogCategoryIDS);
+                    })
+                    ->with('blogCategories')
+                    ->orderBy('date', 'DESC')
+                    ->take($relatedBlogsCount)
+                    ->get();
+    }
+
+    private function getListOfRandomBlogs(
+        int $blogId,
+        string $blogLang,
+        Collection $relatedBlogs,
+        int $missingBlogsCount
+    ): Collection {
+
+        return Blog::where('id', '!=', $blogId)
+                    ->where('lang', $blogLang)
+                    ->whereNotIn('id', $relatedBlogs->pluck('id'))
+                    ->with('blogCategories')
+                    ->inRandomOrder()
+                    ->take($missingBlogsCount)
+                    ->get();
     }
 }
