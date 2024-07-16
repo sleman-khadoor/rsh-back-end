a1496531fd10ba0de83951b84e9ea575203f3557
obada <opadaalziede@gmail.com>
2024-07-17 02:47:19 +0300

    add requires_json middleware globally

---
+++ app/Http/Middleware/RequireJson.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
+
+class RequireJson
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!$request->wantsJson()) {
+
+            return response()->json([
+                'message' => 'Please request with HTTP header: Accept: application/json'
+            ], Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        return $next($request);
+    }
+}
